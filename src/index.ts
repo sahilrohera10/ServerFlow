0da8ad426cb7f60f316d821ddce045cdf308d939
Sahil <sahilrohera10@gmail.com>
2024-06-01 21:28:54 +0530

    [FEAT] load balancer

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import express, { Request, Response } from "express";
+import axios from "axios";
+import schedule from "node-schedule";
+import { GET_A_HEALTHY_SERVER, PERFORM_HEALTH_CHECK } from "./utils/utils";
+const app = express();
+
+const PORT = 8000;
+
+interface session {
+  ip: string;
+  server: string;
+}
+
+export const servers: string[] = [
+  "http://localhost:8001",
+  "http://localhost:8002",
+]; // here we have a list of distributed servers,
+// for which the load balancer shall be implemented
+
+export const sessionMap: { [key: string]: string } = {}; // an object that contains ip as key and a corresponding server address
+export const healthMap: { [key: string]: boolean } = {}; // an object which marks every server with a bool value (true/false) for their health.
+
+// lets map all the servers present in the list as healthy for initial setup
+
+// instead could use forEach as well
+for (let i = 0; i < servers.length; i++) {
+  healthMap[servers[i]] = true;
+}
+
+// perform a health check initially
+PERFORM_HEALTH_CHECK();
+
+// Now schedule a health check process for every 14 mins
+schedule.scheduleJob("*/2 * * * *", PERFORM_HEALTH_CHECK);
+
+// middleware that handles the incoming client's requests
+app.use((req: Request, res: Response) => {
+  // get the client ip address from the request
+  const client_ip = req.socket.remoteAddress || "";
+
+  const server = GET_A_HEALTHY_SERVER(client_ip);
+
+  if (!server) {
+    res.status(503).send("Service unavailable");
+    return;
+  }
+
+  // create the endpoint which is supposed to get hit by client
+  const target_url = `${server}${req.originalUrl}`;
+
+  axios({
+    method: req.method,
+    url: target_url,
+    headers: req.headers,
+    data: req.body,
+  })
+    .then((response) => {
+      return res.status(response.status).json(response.data);
+    })
+    .catch(() => {
+      return res.status(502).send("Bad Gateway");
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`ServerFlow is running on port ${PORT}`);
+});
---
+++ src/utils/utils.ts
@@ -0,0 +1,79 @@
+import axios from "axios";
+import { healthMap, servers, sessionMap } from "..";
+
+// generates a hash code value for a client ip address
+export function GENERATE_IP_HASH(client_ip: string): number {
+  let hash = 0;
+  for (let i = 0; i < client_ip.length; i++) {
+    const char = client_ip.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash |= 0;
+  }
+
+  return hash;
+}
+
+// get a sessioned server or a new alloted server for a client ip address
+export function GET_SERVER_FOR_CLIENT(client_ip: string): string {
+  if (sessionMap[client_ip]) {
+    // if the client is not new and we have its session already then provide this client with the same server.
+    return sessionMap[client_ip];
+  }
+
+  // if the client is new and we dont have its session
+  const server_list_len = servers.length;
+  const serverIndex = Math.abs(GENERATE_IP_HASH(client_ip)) % server_list_len;
+
+  // got a server for the new client
+  const alloted_server = servers[serverIndex];
+
+  // now mapped the client with this server for maintaining a session
+  sessionMap[client_ip] = alloted_server;
+
+  return alloted_server;
+}
+
+// performs the health of the every server present in the list
+export function PERFORM_HEALTH_CHECK() {
+  servers.forEach((server) => {
+    axios
+      .get(`${server}/health`)
+      .then((response) => {
+        if (response.status === 200) {
+          healthMap[server] = true;
+        } else {
+          healthMap[server] = false;
+          console.log(`Unhealthy server => ${server}`);
+        }
+      })
+      .catch(() => {
+        healthMap[server] = false;
+        console.log(`Unhealthy server => ${server}`);
+      });
+  });
+}
+
+// getting a healthy server for a client ip
+
+export function GET_A_HEALTHY_SERVER(client_ip: string) {
+  let server = GET_SERVER_FOR_CLIENT(client_ip);
+
+  // if the server is found to be healthy then directly return that particular server
+  if (healthMap[server]) return server;
+
+  // if the server is unhealthy then we must return some other healthy server
+
+  let attempt = 0;
+  const server_list_len = servers.length;
+
+  while (attempt < server_list_len) {
+    const new_Server_index = (servers.indexOf(server) + 1) % server_list_len;
+    server = servers[new_Server_index];
+
+    if (healthMap[server]) return server;
+
+    attempt++;
+  }
+
+  return null;
+}
